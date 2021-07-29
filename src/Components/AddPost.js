96f92e457d7cee177d53850ae76be535de48bdab
Roksolana <dytchakro@gmail.com>
2021-07-29 10:39:32 +0300

    created one function for submit add and update form

--- src/Components/AddPost.js
+++ src/Components/AddPost.js
@@ -9,17 +9,25 @@ export function AddPost(){
     const post = useSelector((state) => state.app.data)
     const { id }= useParams();
     useEffect(()=>{
-        dispatch(getPost(id))
+        if(id){
+            dispatch(getPost(id))
+        }
     }, [dispatch])
+    const submitForm = (values) => {
+        if(id){
+            dispatch(updatePost(values))
+        } else {
+            dispatch(addPost(values))
+        }
+    }
     return(
         <div>
             <h1>Add Post</h1>
-            <div>{ post.body }</div>
             <Formik enableReinitialize
                 initialValues={{id : id, title: id ? post.title : '', body: id ? post.body : '', userId: id ? post.userId : 0}}
                 onSubmit={(values, actions) => {
                    //dispatch(addPost(values))
-                    dispatch(updatePost(values))
+                    submitForm(values)
                     console.log('work')
                 }}
             >
@@ -27,7 +35,7 @@ export function AddPost(){
                     <Field name="title" placeholder="title" className="input" />
                     <Field name="body" placeholder="body" className="input"/>
                     <Field name='userId' placeholder="userId" className="input"/>
-                    <button type="submit" className="btn btn-submit">Add Post</button>
+                    {id ? <button type="submit" className="btn btn-submit">Update Post</button> : <button type="submit" className="btn btn-submit">Add Post</button> }
                 </Form>
             </Formik>
         </div>
